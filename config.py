c9aabb552f4849c8ec0a9b101ab6a10496b15d92
Royalbot213 <143687906+Royalbot213@users.noreply.github.com>
2025-06-25 17:31:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,15 +35,15 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, get it from https://t.
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Yewsdhi/MUSIC_bot",
+    "https://github.com/Royalbot213/newbababot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/bad_queeno")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/bad_queeno")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+hyjl0Mp7DzU4NTA1")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+BQLv8g4UgVtmMTI1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
